f8f5afb1d0d3d3ad6a01aab81c2c8b3367840be1
gov <gabcomp.gov@gmail.com>
2021-03-24 12:14:52 -0300

    v0.3
    style, fix, refactor, update json_decoder.js

--- json_decoder.js
+++ json_decoder.js
@@ -3,7 +3,7 @@ function json_to_html(){
 
     let json = json_;
 
-    let grid_html = "<div class='container col-xs-4 p-1 my-4 bg-secondary'>";
+    let grid_html = "<div class='container col-xs-4 p-1 my-4 bg-dark'>";
 
     let id = 0;
 
@@ -13,34 +13,12 @@ function json_to_html(){
 
 		for(let j = 0; j < json[1][0][i]; j++){
 
-            let color_bg = (Number(json[1][3][id]) % 10);
-            let color_letter = Number(json[1][3][id]) - color_bg;
-
-            let color_string_bg = "";
-            let color_string_letter = "";
-
-            color_letter = color_letter / 10;
-
-                if(color_bg == 1){  color_string_letter = "light"  }
-                else{               color_string_letter = "dark"   }
-
-                if(color_bg == 0){      color_string_bg = "white"      }
-                else if(color_bg == 1){ color_string_bg = "light"      }
-                else if(color_bg == 2){ color_string_bg = "secondary"  }
-                else if(color_bg == 3){ color_string_bg = "dark"       }
-                else if(color_bg == 4){ color_string_bg = "primary"    }
-                else if(color_bg == 5){ color_string_bg = "success"    }
-                else if(color_bg == 6){ color_string_bg = "danger"     }
-                else if(color_bg == 7){ color_string_bg = "warning"    }
-                else{                   color_string_bg = "info"       }
-
-
             grid_html += "<div class='col-sm-" + json[1][2][id] + "'>";
-            grid_html += "<nav class='navbar navbar-" + color_string_letter + " text-left bg-" + color_string_bg + "'>";
+            grid_html += "<nav class='navbar navbar-light text-left bg-light'>";
 
             grid_html += "<div class='container-fluid'>";
 
-            grid_html += "<div class='container-fluid " + color_string_bg + "' type='button' data-bs-toggle='collapse' data-bs-target='#navbar" + id + "'>";
+            grid_html += "<div class='container-fluid' type='button' data-bs-toggle='collapse' data-bs-target='#navbar" + id + "'>";
 
             grid_html += "<a class='navbar-brand' href='#'>" + json[1][1][id] + "</a></div>";
 
